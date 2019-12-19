a5aa85a0de4af10f48aee0bd02af1ebb29b67b12
ug0 <ug600@icloud.com>
2019-12-19 11:06:15 +0800

    tmp 2019 15

--- 2019/day15/day15.exs
+++ 2019/day15/day15.exs
@@ -3,8 +3,14 @@ Code.load_file("../intcode_program.exs", __DIR__)
 defmodule Day15 do
   alias IntcodeProgram, as: Program
 
+  @starting_point {0, 0}
   def part1(input) do
-    droid = spawn_link(__MODULE__, :find_oxygen_system, [%{{0, 0} => :seen}, {0, 0}, {0, 1}])
+    droid =
+      spawn_link(__MODULE__, :find_oxygen_system, [
+        %{@starting_point => :road},
+        @starting_point,
+        {0, 1}
+      ])
 
     input
     |> Program.new(droid, droid)
@@ -12,33 +18,141 @@ defmodule Day15 do
   end
 
   def part2(input) do
+    droid =
+      spawn_link(__MODULE__, :explore_area, [%{@starting_point => :road}, @starting_point, {0, 1}])
+
+    input
+    |> Program.new(droid, droid)
+    |> Program.execute()
   end
 
-  def find_oxygen_system(area, cursor, direction) do
-    display(area, cursor)
+  def explore_area(area_map, cursor, direction) do
+    IO.inspect({cursor, direction})
     receive do
       {:read_input, remote} ->
         send_move_command(remote, direction)
-        find_oxygen_system(area, cursor, direction)
+        explore_area(area_map, cursor, direction)
 
       {:write_output, value} ->
         new_cursor = vec_add(cursor, direction)
 
         case value do
           _wall = 0 ->
-            area
+            area_map
             |> Map.put(new_cursor, :wall)
-            |> find_oxygen_system(cursor, next_move(area, cursor, direction))
+            |> explore_area(cursor, next_move(area_map, cursor, direction))
 
           1 ->
-            area
-            |> Map.put(new_cursor, :seen)
-            |> find_oxygen_system(new_cursor, direction)
+            case new_cursor do
+              @starting_point ->
+                :done
+                |> IO.inspect()
 
-          _dest = 2 ->
-            IO.inspect(area)
+              _ ->
+                area_map
+                |> Map.put(new_cursor, :road)
+                |> explore_area(new_cursor, direction)
+            end
+
+          _target = 2 ->
+            area_map
+            |> Map.put(new_cursor, :oxygen_system)
+            |> explore_area(new_cursor, direction)
         end
     end
+  end
+
+  def find_oxygen_system(area_map, cursor, direction) do
+    receive do
+      {:read_input, remote} ->
+        send_move_command(remote, direction)
+        find_oxygen_system(area_map, cursor, direction)
+
+      {:write_output, value} ->
+        new_cursor = vec_add(cursor, direction)
+
+        case value do
+          _wall = 0 ->
+            area_map
+            |> Map.put(new_cursor, :wall)
+            |> find_oxygen_system(cursor, random_move(area_map, cursor, direction))
+
+          1 ->
+            area_map
+            |> Map.put(new_cursor, :road)
+            |> find_oxygen_system(new_cursor, direction)
+
+          _target = 2 ->
+            area_map
+            |> Map.put(new_cursor, :oxygen_system)
+            |> display({0, 0})
+            |> fewest_moves_to_oxygen_system({0, 0})
+            |> IO.inspect()
+
+            Process.exit(self(), :kill)
+        end
+    end
+  end
+
+  defp fewest_moves_to_oxygen_system(map, from) do
+    {to, _} = Enum.find(map, &(elem(&1, 1) == :oxygen_system))
+
+    map
+    |> shortest_path(from, to)
+    |> length()
+  end
+
+  defp shortest_path(map, from, to) do
+    map
+    |> find_paths(from, to, nil, [])
+    |> Enum.min_by(&length/1)
+  end
+
+  defp find_paths(_map, to, to, _prev, path) do
+    [path]
+  end
+
+  defp find_paths(map, {x, y} = from, to, prev, path) do
+    [
+      {x + 1, y},
+      {x - 1, y},
+      {x, y + 1},
+      {x, y - 1}
+    ]
+    |> Stream.reject(&(&1 == prev))
+    |> Stream.filter(&(map[&1] in [:road, :oxygen_system]))
+    |> Enum.flat_map(fn coord ->
+      find_paths(map, coord, to, from, [coord | path])
+    end)
+  end
+
+  defp next_move(area_map, cursor, direction) do
+    case {
+      # right
+      area_map[vec_add(cursor, turn_right(direction))],
+      # forward
+      area_map[vec_add(cursor, direction)],
+      # left
+      area_map[vec_add(cursor, turn_left(direction))]
+    } do
+      {:wall, :wall, _} -> turn_left(direction)
+      {:wall, _, _} -> direction
+      _ -> turn_right(direction)
+    end
+  end
+
+  defp turn_left({x, y}), do: {-y, x}
+  defp turn_right({x, y}), do: {y, -x}
+
+  defp random_move(area_map, cursor, _) do
+    [
+      {0, 1},
+      {0, -1},
+      {1, 0},
+      {-1, 0}
+    ]
+    |> Enum.filter(fn v -> area_map[vec_add(cursor, v)] != :wall end)
+    |> Enum.random()
   end
 
   defp send_move_command(remote, {0, 1}), do: send(remote, 1)
@@ -50,8 +164,8 @@ defmodule Day15 do
     {x + i, y + j}
   end
 
-  defp display(area, droid) do
-    points = Map.keys(area)
+  defp display(area_map, droid) do
+    points = Map.keys(area_map)
     {{min_x, _}, {max_x, _}} = Enum.min_max_by(points, fn {x, _} -> x end)
     {{_, min_y}, {_, max_y}} = Enum.min_max_by(points, fn {_, y} -> y end)
 
@@ -59,23 +173,28 @@ defmodule Day15 do
     |> Enum.each(fn y ->
       min_x..max_x
       |> Enum.map(fn x ->
-        case Map.get(area, {x, y}) do
-          :wall ->
-            ?#
+        if {x, y} == droid do
+          ?D
+        else
+          case Map.get(area_map, {x, y}) do
+            :wall ->
+              ?#
 
-          :seen ->
-            ?.
+            :road ->
+              ?.
 
-          _ ->
-            if {x, y} == droid do
-              ?D
-            else
+            :oxygen_system ->
+              ?O
+
+            _ ->
               ?\s
-            end
+          end
         end
       end)
       |> IO.puts()
     end)
+
+    area_map
   end
 end
 
