30c91e07825c37f583f60cf5ae39a530939654ff
ug0 <ug600@icloud.com>
2020-12-23 11:10:29 +0800

    Improve 2020 17

--- lib/aoc/2020/17/solution.ex
+++ lib/aoc/2020/17/solution.ex
@@ -1,134 +1,81 @@
 defmodule Aoc.Y2020.D17 do
   use Aoc.Input
 
-  defmodule Cubes do
-    defstruct [:active_set, :range]
-
-    @active ?#
-    @inactive ?.
-
-    def new(coords, dimension \\ 3) do
-      coords =
-        Enum.map(coords, fn {coord, state} ->
-          {expand_coord_dimension(coord, dimension), state}
-        end)
-
-      range =
-        Enum.map(0..(dimension - 1), fn i ->
-          {{min, _}, {max, _}} = Enum.min_max_by(coords, fn {coord, _} -> elem(coord, i) end)
-          elem(min, i)..elem(max, i)
-        end)
-
-      active_set =
-        coords
-        |> Stream.filter(fn {_, state} -> state == @active end)
-        |> Stream.map(fn {coord, _} -> coord end)
-        |> MapSet.new()
-
-      %__MODULE__{active_set: active_set, range: range}
-    end
-
-    defp expand_coord_dimension(coord, dimension) do
-      case tuple_size(coord) do
-        ^dimension -> coord
-        n when n < dimension -> coord |> Tuple.append(0) |> expand_coord_dimension(dimension)
-      end
-    end
-
-    def active_state_count(%__MODULE__{active_set: set}) do
-      MapSet.size(set)
-    end
-
-    def cycle(%__MODULE__{} = cubes, 0) do
-      cubes
-    end
-
-    def cycle(%__MODULE__{} = cubes, times) do
-      cubes
-      |> scale(1)
-      |> change_states()
-      |> cycle(times - 1)
-    end
-
-    defp scale(%__MODULE__{range: range} = cubes, n) do
-      %{cubes | range: Enum.map(range, fn min..max -> (min - n)..(max + n) end)}
-    end
-
-    defp change_states(%__MODULE__{} = cubes) do
-      %{cubes | active_set: next_active_set(cubes)}
-    end
-
-    defp next_active_set(%__MODULE__{range: [x_range, y_range, z_range]} = cubes) do
-      for x <- x_range, y <- y_range, z <- z_range, reduce: MapSet.new() do
-        acc ->
-          if next_state(cubes, coord = {x, y, z}) == @active do
-            MapSet.put(acc, coord)
-          else
-            acc
-          end
-      end
-    end
-
-    defp next_active_set(%__MODULE__{range: [x_range, y_range, z_range, w_range]} = cubes) do
-      for x <- x_range, y <- y_range, z <- z_range, w <- w_range, reduce: MapSet.new() do
-        acc ->
-          if next_state(cubes, coord = {x, y, z, w}) == @active do
-            MapSet.put(acc, coord)
-          else
-            acc
-          end
-      end
-    end
-
-    defp next_state(%__MODULE__{active_set: set}, coord) do
-      case {MapSet.member?(set, coord), active_neighbors_count(set, coord)} do
-        {true, c} when c in 2..3 -> @active
-        {true, _} -> @inactive
-        {_, 3} -> @active
-        _ -> @inactive
-      end
-    end
-
-    defp active_neighbors_count(set, coord) do
-      coord
-      |> neighbors()
-      |> Enum.count(&MapSet.member?(set, &1))
-    end
-
-    defp neighbors({x, y, z}) do
-      for i <- -1..1, j <- -1..1, k <- -1..1, i != 0 or j != 0 or k != 0 do
-        {x + i, y + j, z + k}
-      end
-    end
-
-    defp neighbors({x, y, z, w}) do
-      for i <- -1..1, j <- -1..1, k <- -1..1, l <- -1..1, i != 0 or j != 0 or k != 0 or l != 0 do
-        {x + i, y + j, z + k, w + l}
-      end
-    end
-  end
-
-  alias Aoc.Y2020.D17.Cubes
+  @active ?#
 
   def part1(str \\ nil) do
     (str || input())
-    |> parse_input()
-    |> Cubes.new(3)
-    |> Cubes.cycle(6)
-    |> Cubes.active_state_count()
+    |> solve(3)
     |> IO.inspect()
   end
 
   def part2(str \\ nil) do
     (str || input())
-    |> parse_input()
-    |> Cubes.new(4)
-    |> Cubes.cycle(6)
-    |> Cubes.active_state_count()
+    |> solve(4)
     |> IO.inspect()
   end
 
-  defp parse_input(str) do
+  defp solve(str, dimension) do
+    str
+    |> parse_initial_active_set()
+    |> Enum.into(MapSet.new(), &expand_dimension(&1, dimension))
+    |> cycle(6)
+    |> MapSet.size()
+  end
+
+  defp cycle(set, 0) do
+    set
+  end
+
+  defp cycle(set, times) do
+    set
+    |> new_active_set()
+    |> cycle(times - 1)
+  end
+
+  defp new_active_set(set) do
+    set
+    |> Stream.flat_map(&neighbors/1)
+    |> Stream.uniq()
+    |> Stream.filter(&is_active_in_next_cycle?(set, &1))
+    |> Enum.into(MapSet.new())
+  end
+
+  defp is_active_in_next_cycle?(set, coord) do
+    case {MapSet.member?(set, coord), active_neighbors_count(set, coord)} do
+      {true, c} when c in 2..3 -> true
+      {true, _} -> false
+      {_, 3} -> true
+      _ -> false
+    end
+  end
+
+  defp active_neighbors_count(set, coord) do
+    coord
+    |> neighbors()
+    |> Enum.count(&MapSet.member?(set, &1))
+  end
+
+  defp expand_dimension(coord, dimension) do
+    case tuple_size(coord) do
+      ^dimension -> coord
+      n when n < dimension -> coord |> Tuple.append(0) |> expand_dimension(dimension)
+    end
+  end
+
+  defp neighbors({x, y, z}) do
+    for i <- -1..1, j <- -1..1, k <- -1..1, i != 0 or j != 0 or k != 0 do
+      {x + i, y + j, z + k}
+    end
+  end
+
+  defp neighbors({x, y, z, w}) do
+    for i <- -1..1, j <- -1..1, k <- -1..1, l <- -1..1, i != 0 or j != 0 or k != 0 or l != 0 do
+      {x + i, y + j, z + k, w + l}
+    end
+  end
+
+  defp parse_initial_active_set(str) do
     str
     |> String.splitter("\n", trim: true)
     |> Stream.with_index()
@@ -136,10 +83,11 @@ defmodule Aoc.Y2020.D17 do
       line
       |> to_charlist()
       |> Stream.with_index()
-      |> Enum.map(fn {state, x} ->
-        {{x, y}, state}
+      |> Stream.filter(fn {state, _} -> state == @active end)
+      |> Enum.map(fn {_, x} ->
+        {x, y}
       end)
     end)
-    |> Enum.into(%{})
+    |> Enum.into(MapSet.new())
   end
 end
